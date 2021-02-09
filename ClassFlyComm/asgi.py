461d42673d5806c20599071cf3deb8aac7628424
ravichandrareddy88@gmail.com <ravichandrareddy88@gmail.com>
2021-02-09 15:26:39 +0530

    good to https

--- ClassFlyComm/asgi.py
+++ ClassFlyComm/asgi.py
@@ -12,7 +12,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClassFlyComm.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
-  "http": AsgiHandler(),
+  "https": AsgiHandler(),
   "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
