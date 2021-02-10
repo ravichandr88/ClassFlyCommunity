1c81029300aea7e6e92fee5f8edf7537e7a96955
ravichandrareddy88@gmail.com <ravichandrareddy88@gmail.com>
2021-02-10 16:51:31 +0530

    god

--- ClassFlyComm/settings.py
+++ ClassFlyComm/settings.py
@@ -115,7 +115,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('rediss://:pe8586157d58d6c7e8d69c9879829d808b16354b8a7a87dfd976a82125845a61b@ec2-108-128-33-61.eu-west-1.compute.amazonaws.com:15710',6379)],
+            "hosts": [('rediss://:pe8586157d58d6c7e8d69c9879829d808b16354b8a7a87dfd976a82125845a61b@ec2-108-128-33-61.eu-west-1.compute.amazonaws.com:15710',6480)],
         },
     },
 }
