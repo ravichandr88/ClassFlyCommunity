bf366bcd232cc35a2234fe04523488567774f7e3
ravichandrareddy88@gmail.com <ravichandrareddy88@gmail.com>
2021-09-06 20:53:04 +0530

    chat room table updated

--- pro/views.py
+++ pro/views.py
@@ -289,7 +289,7 @@ def resend_otp(request):
 
 def login_view(request): 
     #IF the user is already logged in 
-    if request.user == 'AnonymousUser':
+    if request.user != 'AnonymousUser':
         return redirect('pro_home')
 
     if request.method == 'POST':
