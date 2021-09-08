43bfd0fe00598001e89ea40153e53b37a0c575fd
ravichandrareddy88@gmail.com <ravichandrareddy88@gmail.com>
2021-09-08 14:16:40 +0530

    chatting images and method fixed

--- chatt/views.py
+++ chatt/views.py
@@ -3,6 +3,8 @@ from django.views.generic import TemplateView
 from django.shortcuts import HttpResponse, Http404
 from django.contrib.auth.models import User
 from django.db.models import Q
+from interview.models import Prfessional, Fresher, HRaccount
+
 
 # chat/views.py
 from django.shortcuts import render
@@ -41,16 +43,37 @@ def chatting(request, room_name):
     room = TwoGroup.objects.get(channel_name = room_name)
 
     user_id = ''
-
+    img_url = ''
+    oppo_user = ''  #opposite user, later get the img_url
+    messages = room.chats.all().order_by('-created_on')[:20]
+# user position
+# try to get the profile pic url for opposite chat
+  
     if room.prof == user:
         user_id = 'prof'
+        # get the img_url of the opposite person
+        oppo_user = room.fresher
+        
     elif room.fresher == user:
         user_id = 'fresher'
+        oppo_user = room.prof
+        
     else:
         print(room.prof, room.fresher, user)
         raise Http404
+    
+    # get the img_url for the opposite user
+    if Prfessional.objects.filter(user = oppo_user).count() == 1:
+        img_url = Prfessional.objects.get(user = oppo_user).profile_pic
 
-    return render(request, 'chat.html', context={'room_name': room_name,'user_id':user_id})
+    elif Fresher.objects.filter(user = oppo_user).count()  == 1:
+        img_url = Fresher.objects.get(user = oppo_user).profile_pic
+
+    elif HRaccount.objects.filter(user = oppo_user).count() == 1:
+        img_url = HRaccount.objects.get(user = oppo_user).profilepic
+
+    
+    return render(request, 'chat.html', context={'room_name': room_name,'user_id':user_id,'img_url':img_url,'messages':messages,'room':room})
 
 
 
